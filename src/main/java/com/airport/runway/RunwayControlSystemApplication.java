9ebbbdb657699311f7a94c611a476e61ecbc12a7
Your Name <you@example.com>
2024-12-11 22:45:21 +0800

    Initial commit

---
+++ src/main/java/com/airport/runway/RunwayControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.airport.runway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunwayControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunwayControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airport/runway/RunwayControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airport.runway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunwayControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
