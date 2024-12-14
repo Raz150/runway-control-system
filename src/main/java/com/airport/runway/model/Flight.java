57b67121a38fcd5f6b5feb2193b13644316dd0a9
Adzrul D <adzruldani@gmail.com>
2024-12-14 15:41:35 +0800

    fix:added setter and getter for Plane

--- src/main/java/com/airport/runway/model/Flight.java
+++ src/main/java/com/airport/runway/model/Flight.java
@@ -2,6 +2,8 @@ package com.airport.runway.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalTime;
+
 @Entity
 public class Flight {
     @Id
@@ -19,16 +21,72 @@ public class Flight {
     private String flightStatus;
     private String departureFrom;
     private String arrivalAt;
-    private String scheduledDeparture;
-    private String scheduledArrival;
+    private LocalTime scheduledDeparture;
+    private LocalTime scheduledArrival;
 
     public Flight(){}
 
-    public Flight(Plane plane, Runway runway, String flightStatus, String departureFrom, String arrivalAt, String scheduledDeparture, String scheduledArrival){
+    public Flight(Plane plane, Runway runway, String flightStatus, String departureFrom, String arrivalAt, LocalTime scheduledDeparture, LocalTime scheduledArrival){
         this.flightStatus = flightStatus;
         this.departureFrom = departureFrom;
         this.arrivalAt = arrivalAt;
         this.scheduledDeparture = scheduledDeparture;
+        this.scheduledArrival = scheduledArrival;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
+
+    public Runway getRunway() {
+        return runway;
+    }
+
+    public void setRunway(Runway runway) {
+        this.runway = runway;
+    }
+
+    public String getFlightStatus() {
+        return flightStatus;
+    }
+
+    public void setFlightStatus(String flightStatus) {
+        this.flightStatus = flightStatus;
+    }
+
+    public String getDepartureFrom() {
+        return departureFrom;
+    }
+
+    public void setDepartureFrom(String departureFrom) {
+        this.departureFrom = departureFrom;
+    }
+
+    public String getArrivalAt() {
+        return arrivalAt;
+    }
+
+    public void setArrivalAt(String arrivalAt) {
+        this.arrivalAt = arrivalAt;
+    }
+
+    public LocalTime getScheduledDeparture() {
+        return scheduledDeparture;
+    }
+
+    public void setScheduledDeparture(LocalTime scheduledDeparture) {
+        this.scheduledDeparture = scheduledDeparture;
+    }
+
+    public LocalTime getScheduledArrival() {
+        return scheduledArrival;
+    }
+
+    public void setScheduledArrival(LocalTime scheduledArrival) {
         this.scheduledArrival = scheduledArrival;
     }
 }
--- src/main/java/com/airport/runway/services/FlightStatusService.java
+++ src/main/java/com/airport/runway/services/FlightStatusService.java
@@ -1,6 +1,8 @@
 package com.airport.runway.services;
 
 import com.airport.runway.enums.FlightStatus;
+import com.airport.runway.model.Flight;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -8,7 +10,20 @@ import java.util.List;
 
 @Service
 public class FlightStatusService {
-    public List<FlightStatus> getAllFlightStatus(){
-        return Arrays.asList(FlightStatus.values());
+    private Flight flight;
+    private List<FlightStatus> flightStatuses = Arrays.asList(FlightStatus.values());
+
+    @Autowired
+    public FlightStatusService(Flight flight){
+        this.flight = flight;
+    }
+//    public List<FlightStatus> getAllFlightStatus(){
+//        return Arrays.asList(FlightStatus.values());
+//    }
+
+    // Method to change flight status sequently
+    public void changeStatus(Flight flight){
+        // Get current status
+//        FlightStatus currentStatus = flight.get
     }
 }
