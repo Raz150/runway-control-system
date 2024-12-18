7456b50bb7943d43daf6e4a497a7070ffb8c9f19
Your Name <you@example.com>
2024-12-18 21:38:40 +0800

    fix conflict error

--- src/main/java/com/airport/runway/controllers/FlightController.java
+++ src/main/java/com/airport/runway/controllers/FlightController.java
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.*;
 import com.airport.runway.model.Flight;
 import com.airport.runway.services.ArrivalService;
 
+import java.util.List;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.airport.runway.services.ArrivalService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
